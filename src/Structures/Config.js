7eb4243251b1972da2b727e14729c406d7a97000
Demon-King169 <161844189+Demon-King169@users.noreply.github.com>
2024-03-11 12:39:32 +0530

    Update Config.js

--- src/Structures/Config.js
+++ src/Structures/Config.js
@@ -3,7 +3,7 @@ module.exports = {
     intents: ['32767'] // To learn more about intents, go to Discord Developer Portal.
   },
   guildOnly: {
-    enabled: false,
-    guildID: '' // Guild ID
+    enabled: true,
+    guildID: '1213033696261570631' // Guild ID
   } // If set to true, slash commands will only be registered only under the guild with specified guild ID; Else globally registered.
 };
