0d877c58d8bf70b6a1af306e063f03da7f77d7a0
Demon-King169 <161844189+Demon-King169@users.noreply.github.com>
2024-03-11 12:24:26 +0530

    Update Ready.js

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -3,7 +3,7 @@ const { plural } = require('../Structures/Utils');
 module.exports = {
   event: 'ready',
   async run(bot) {
-    bot.user.setPresence({ activities: [{ name: `the world`, type: 'WATCHING' }] });
+    bot.user.setPresence({ activities: [{ name: `Your Commands | Powered By ShadowStriker`, type: 'WATCHING' }] });
     bot.logger.success(`${bot.user.tag} is now logged in!`);
     bot.logger.info(
       `Loaded ${bot.commands.size} commands for ${bot.guilds.cache.size} guild${plural(bot.guilds.cache.size)}.`
